3ac9a0148351cc909f211ce3a24beb054d77558c
pExplorerGH <155173730+pExplorerGH@users.noreply.github.com>
2023-12-29 19:07:17 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from browser_window import BrowserWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    app.setApplicationName("Simple Browser")
+
+    window = BrowserWindow()
+    window.show()
+
+    sys.exit(app.exec())
